b39ca2648d5c2242029d4dd257a614196b647ac5
Asangika <asangikanethmini@gmail.com>
2024-08-30 17:56:16 +0530

    start develop

--- webprodigies-plura/src/middleware.ts
+++ webprodigies-plura/src/middleware.ts
@@ -1,8 +1,7 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware({
-    publicRoutes: ['/site', '/api/uploadthing'],
-})
+export default clerkMiddleware()
+
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
