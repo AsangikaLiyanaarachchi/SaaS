c0848a7415efc1a9cf72e8c6eb6f51f98f261290
Asangika <asangikanethmini@gmail.com>
2024-09-30 10:05:26 +0530

    2h-3h

---
+++ webprodigies-plura/src/app/(main)/agency/unauthorized/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Unauthorized from '@/components/unauthorized'
+type Props={}
+
+const page = (props: Props) => {
+  return <Unauthorized />
+}
+
+export default page
